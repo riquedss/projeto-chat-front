bbd69de2fe89ffb3afc03a386db6080375fefae0
Henrique Santana <hsantana.sousass@gmail.com>
2022-02-19 00:38:27 -0300

    Add funcao de editar mensagem

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 const url = "http://treinamento-ajax-api.herokuapp.com/messages"
 
 function mostraMensagemNaTela(mensagem) {
-    const tela = document.querySelector(".mensagem")
+    const tela = document.querySelector(".mensagem-caixa")
 
     //Printa todas as mensagens da Api na tela
     for (objeto of mensagem) {
         
         const div = document.createElement("div")
         div.classList.add("guarda-mensagem")
-        div.innerHTML = `<section class="mensagem-tela"><p">${objeto.content}</p></section>
-                    <section class="section-btn">
-                    <input type="button" value="deletar" class="btn-deletar" onclick="deletar(this)">
-                    <input type="button" value="editar" class="btn-editar" onclick="editar(this)">
-                </section></section>`
+        div.innerHTML = `
+        <section class="mensagem-tela"> 
+            <p class="p1">${objeto.author}:</p>
+             <p class="p2">${objeto.content}</p>
+        </section>
+        <section class="section-btn">
+            <input type="button" value="deletar" class="btn-deletar" onclick="deletar(this)">
+            <input type="button" value="editar" class="btn-editar" onclick="editar(this)">
+        </section></section>`
         tela.appendChild(div)
         div.id = `${objeto.id}`
     }
 }
 
 function obterMensagemDaApi() {
-    const tela = document.querySelector(".mensagem")
+    const tela = document.querySelector(".mensagem-caixa")
     tela.innerHTML = "";
 
     fetch(url).then(response => {
         response.json().then(todasMensagens => {
-            //Código para mostrar em html
             mostraMensagemNaTela(todasMensagens);
         }).catch(err => console.log(err))
-    }).catch(err => console.log(err)).finally(() => console.log("Sem erro de execução"))
+    }).catch(err => console.log(err)).finally(() => console.log("Executou!"))
 
 }
 
@@ -37,7 +40,7 @@ function enviar() {
     const fetchBody = {
         message: { 
             content: texto.value,
-            author: "Ronaldinho"
+            author: "Lobinho" //bug só está m
         }
     }
 
@@ -52,13 +55,9 @@ function enviar() {
             obterMensagemDaApi();
             console.log(mensagem);
         }).catch(err => console.log(err))
-    }).catch(err => console.log(err)).finally(() => console.log("Sem erro de execução"))
+    }).catch(err => console.log(err))
 
     texto.value = ""
-    
-
-    
- 
 }
 
 // Apagar na Api
@@ -69,45 +68,55 @@ function deletar(elemento){
     //id da mensagem para ser apagada
     const id = elemento.parentElement.parentElement.id;
 
-    fetch(url + "/"+ id, fetchConfig).then(() => {
-        obterMensagemDaApi();
-        console.log("Apagou!");
-    }).catch(err => console.log(err)).finally(() => console.log("Sem erro de execução"));
+    fetch(url + "/"+ id, fetchConfig).then(mensagem => {
+            obterMensagemDaApi();
+            console.log(mensagem);
+    }).catch(err => console.log(err))
     
+}
+
+function editar(elemento){
+    const caixaDeMensagem = elemento.parentElement.parentElement
+    // console.log(caixaDeMensagem)
+    caixaDeMensagem.innerHTML = `
+        <section class="mensagem-tela"> 
+        <form class="form2">
+        <textarea class="texto-edicao" placeholder="Edite a mensagem aqui"></textarea>
+        </form>
+        </section>
+        <section class="section-btn">
+            <input type="button" value="enviar" class="btn-enviar-edicao" onclick="enviarEdicao(this)">
+        </section></section>`
+}
+
+function enviarEdicao(elemento){
+    const textoEdicao = document.querySelector(".texto-edicao").value;
+    //id da mensagem para ser editada
+    const id = elemento.parentElement.parentElement.id;
+    
+
+    const fetchBody = {
+        message: { 
+            content: textoEdicao.value,
+            author: "pirulito" //bug só está editando quando coloca author
+        }
+    }
+
+    const fetchConfig = {
+        method: "PUT",
+        body: JSON.stringify(fetchBody),
+        headers: {"Content-Type":"application/json"}
+    }
+
+    fetch(url + "/" + id, fetchConfig).then(response => {
+        response.json().then(mensagem => {
+            obterMensagemDaApi();
+            console.log(mensagem);
+        }).catch(err => console.log(err))
+    }).catch(err => console.log(err))
     
 
 }
+
+
 obterMensagemDaApi();
-
-
-// botao.addEventListener("click", (elemento) => {
-//     console.log(elemento)
-//     const mensagem = document.querySelector(".mensagem")
-//     const text = document.querySelector(".texto")
-
-//     const div = document.createElement("div")
-//     div.classList.add("guarda-mensagem")
-//     div.innerHTML = `<section class="mensagem-tela"><p">${text.value}</p></section>
-//                     <section class="section-btn">
-//                     <input type="button" value="deletar" class="btn-deletar" onclick="deletar(this)">
-//                     <input type="button" value="editar" class="btn-editar" onclick="editar(this)">
-//                 </section></section>`
-//     text.value = "";
-
-
-//     mensagem.appendChild(div)
-// })
-
-
-
-
-// function editar(elemento){
-   
-//     elemento.parentElement.parentElement.innerHTML = `<section class="mensagem-tela"></section>
-//     <section class="section-btn">
-//     <input type="button" value="deletar" class="btn-deletar" onclick="deletar(this)">
-//     <input type="button" value="enviar" class="botao-enviar">
-// </section></section>`
-
-// }
-
